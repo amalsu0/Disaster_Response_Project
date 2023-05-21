f199da3a8a42e99bc4a1530a98820e5fddabf83b
Amal <73324153+amalsu0@users.noreply.github.com>
2023-05-22 01:44:15 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -76,7 +76,7 @@ def save_data(df, database_filename):
         database_filename:  path to create engine
     """
     engine = create_engine(f"sqlite:///{database_filename}")
-    df.to_sql("disaster_response" , engine, index=False)
+    df.to_sql("disaster_response" , engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
